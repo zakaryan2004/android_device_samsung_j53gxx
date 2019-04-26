634b0ba16596c9163450c0c36e54fe0219d5e57a
Gegham Zakaryan <zakaryan.2004@outlook.com>
2019-04-26 15:36:16 +0400

    j53gxx: Pixel Experience bringup
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,5 @@ LOCAL_PATH := device/samsung/j53gxx
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_j53gxx.mk \
 	$(LOCAL_DIR)/pa_j53gxx.mk \
-	$(LOCAL_DIR)/lineage_j53gxx.mk
+	$(LOCAL_DIR)/lineage_j53gxx.mk \
+	$(LOCAL_DIR)/aosp_j53gxx.mk
