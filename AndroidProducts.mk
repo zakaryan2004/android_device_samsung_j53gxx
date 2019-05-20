19e696694bcb69b6b09087f867bd5d2316716def
Gegham Zakaryan <zakaryan.2004@outlook.com>
2019-05-20 17:02:40 +0400

    j53gxx: Omni bringup
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,4 +3,5 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_j53gxx.mk \
 	$(LOCAL_DIR)/pa_j53gxx.mk \
 	$(LOCAL_DIR)/lineage_j53gxx.mk \
-	$(LOCAL_DIR)/aosp_j53gxx.mk
+	$(LOCAL_DIR)/aosp_j53gxx.mk \
+	$(LOCAL_DIR)/omni_j53gxx.mk 
